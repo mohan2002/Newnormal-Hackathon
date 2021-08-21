6b856a0f72ccf52785fcf1d04c79a0da76efe2a7
Mohan <mohanprasaath269@gmail.com>
2021-08-21 21:19:08 +0530

    Starterpage-update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import StarterPage from './Pages/StarterPage';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import Login from './Pages/Login';
+
 
 function App() {
   return (
     <div className="App">
-       
+       <Router>
+         <Switch>
+            <Route exact path="/" component={StarterPage}/>
+            <Route path="/login" component={Login}/>
+         </Switch>
+       </Router>
     </div>
   );
 }
---
+++ src/Pages/StarterPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import logo from '../Images/Group 1.png'
+
+function StarterPage() {
+    return (
+        <div>
+            <img src={logo}></img>
+            <h1>WELCOME</h1>
+            <p>We help Students/users share informations,resources ask queries and make learning easier in certain ways!!</p>
+            <Link to="/login">LOGIN</Link>
+            <Link>SIGNUP</Link>
+        </div>
+    )
+}
+
+export default StarterPage
